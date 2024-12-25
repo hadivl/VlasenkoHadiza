6ea5aaea4518669108f6cca03a2179f833ea6cbf
Хадижа В <140256920+hadivl@users.noreply.github.com>
2024-12-25 12:06:53 +0300

    Add files via upload

---
+++ Vlasenko/Vlasenko/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+
+// Формат файла выбрать самостоятельно. В главной функции создать объект класса TouristicOperator и заполнить массив в диалоговом режиме
+//(предварительно запросив размер массива у пользователя). Далее отсортировать массив. Вывести результат в файл.
+
+namespace Vlasenko
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TouristicOperator touristicOperator = new TouristicOperator();
+
+            Console.WriteLine("Введите размер массива: ");
+
+            int count = 0;
+            while (!int.TryParse(Console.ReadLine(), out count) || count <= 0)
+            {
+                Console.WriteLine("Введите корректное число для массива");
+
+            }
+
+            for (int i = 0; i <= count; i++)
+            {
+                Console.Write($"Массив: {i + 1}");
+            
+
+
+            Console.Clear();
+            Console.WriteLine("Введите продолжительность поездки: ");
+            string prodol = Console.ReadLine();
+
+            Console.WriteLine("Введите цену поездки:");
+            double prise;
+            while (!double.TryParse(Console.ReadLine(), out prise) || prise < 0)
+            {
+                Console.WriteLine("Введите корректное число для цены поездки");
+
+            }
+
+            Console.WriteLine("Введите размер группы:");
+            int razmer;
+            while (!int.TryParse(Console.ReadLine(), out razmer) || razmer < 0)
+            {
+                Console.WriteLine("Введите корректное число для размера группы");
+
+            }
+
+            Trip trip = new Trip(prodol, prise, razmer);
+            touristicOperator.AddTrip(trip);
+
+            touristicOperator.Sortirovka();
+        }
+
+            //Console.WriteLine("Введите имя файла:");
+            string filename = "filename1.txt";
+            touristicOperator.SaveToFile(filename);
+            Console.WriteLine("Файл успешно сохранен:");
+
+        }
+
+        
+
+    }
+}
---
+++ Vlasenko/Vlasenko/Trip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Описать тип(структуру или класс) Trip для хранения данных о туристической поездке (свойства: продолжительность - строка, цена - число, размер туристической группы - число).
+
+namespace Vlasenko
+{
+    public class Trip
+    {
+        public string Prodolzhitelnost { get; set; }
+        public double Prise { get; set; }
+        public int Razmer { get; set; }
+
+
+        public Trip(string prodol, double prise, int razmer)
+        {
+            Prodolzhitelnost = prodol;
+            Prise = prise;
+            Razmer = razmer;
+        }
+
+        public override string ToString()
+        {
+            return $"Продолжительность: {Prodolzhitelnost}, Цена: {Prise}, Размер группы: {Razmer} ";
+        }
+    }
+}
